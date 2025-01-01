0293753569f6dad7f543aba9fac174b2ad530a95
ghchen99 <ghchen10@outlook.com>
2025-01-01 22:07:43 +0000

    folder structure update

--- train.py
+++ train.py
@@ -1,25 +1,25 @@
 import os
-
 import logging
 from src.HDF5Reader import read_from_hdf5
 
-def main():
-    """
-    Main function to load and inspect HDF5 data.
-    """
+def resahape_action():
     input_file = 'data/dataset/train/train_features.h5'
 
     if os.path.exists(input_file):
         actions = read_from_hdf5(input_file)
         
         for index, action in enumerate(actions):
-            logging.info(f"Action ID: {index}")
-            logging.info(f"Body part: {action['bodypart']}")
+            action_ID = index
+            bodypart = action['bodypart']
+            
             for clip in action['clips']:
                 camera_angle = clip['Camera type']
                 video_features = clip['video_features']
     else:
         logging.error(f"File not found: {input_file}")
+
+def main():
+    resahape_action()    
 
 if __name__ == "__main__":
     main()
