56ec5a22886bdb7b1a4b2f6f5849e5cf9311428f
ghchen99 <ghchen10@outlook.com>
2025-01-02 09:50:16 +0000

    variables saved before reshaping actionns

--- train.py
+++ train.py
@@ -8,13 +8,19 @@ def resahape_action():
     if os.path.exists(input_file):
         actions = read_from_hdf5(input_file)
         
-        for index, action in enumerate(actions):
-            action_ID = index
-            bodypart = action['bodypart']
+        for action in actions:
+            actionclass = action['actionclass'] # type bytes
+            bodypart = action['bodypart'] # type bytes
+            offence = action['offence'] # type bytes
+            severity = action['severity'] # type bytes
+            touchball = action['touchball'] # type bytes
+            trytoplay = action['trytoplay'] # type bytes
             
             for clip in action['clips']:
-                camera_angle = clip['Camera type']
-                video_features = clip['video_features']
+                camera_angle = clip['Camera type'] # type bytes
+                replay_speed = clip['Replay speed'] # numpy.float64
+                video_features = clip['video_features'] # type torch.Tensor (1, 512)
+                continue
     else:
         logging.error(f"File not found: {input_file}")
 
